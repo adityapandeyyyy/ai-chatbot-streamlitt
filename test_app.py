7be8de17f9f3cc29a209700c0fcc6df49c190f8b
Aditya <86356728+adityapandeyyyy@users.noreply.github.com>
2026-03-16 04:15:13 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ from sentence_transformers import SentenceTransformer
 # CONFIG
 # -----------------------------
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # -----------------------------
 # LOAD DATA
